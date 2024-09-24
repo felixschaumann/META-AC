e9c2dd940a5aedfc21521eec389fcd86f3e93cc6
Felix Schaumann <felix.schaumann@mpimet.mpg.de>
2024-09-24 13:43:18 +0200

    update AMOC_Carbon variable names

--- src/components/AMOC_Carbon.jl
+++ src/components/AMOC_Carbon.jl
@@ -1,29 +1,29 @@
 @defcomp AMOC_Carbon begin
 
     # Variables
-    AMOC_decrease = Variable(index=[time], unit="Sv") # AMOC decrease (measured in Sv) starting in t=0
-    AMOC_strength = Variable(index=[time], unit="Sv") # AMOC strength (measured in Sv) starting in t=0
-    CO2_AMOC = Variable(index=[time], unit="GtCO2")
+    AMOC_decrease = Variable(index=[time], unit="Sv") # AMOC decrease (Sv) - a positive value indicates a decrease in AMOC strength
+    AMOC_strength = Variable(index=[time], unit="Sv") # AMOC strength (Sv)
+    CO2_AMOC = Variable(index=[time], unit="GtCO2") # CO2 release due to AMOC decrease (GtCO2)
     cum_CO2_AMOC = Variable(index=[time], unit="GtCO2")
     
     # Parameters
     T_AT = Parameter(index=[time], unit="degC")
-    std_AMOC_strength = Parameter(index=[time], default=ones(451), unit="Sv") # standard deviation of AMOC strength in a given projection
+    std_AMOC_strength = Parameter(index=[time], default=zeros(451), unit="Sv") # standard deviation of AMOC strength in a given projection
     
     beta_AMOC = Parameter(index=[time]) # coefficient representing the sensitivity of AMOC to temperature
     eta_AMOC = Parameter() # coefficient representing the sensitivity of the carbon drawdown to AMOC decrease
     
-    T_AT_2010 = Parameter(default=0.854, unit="degC")
-    AMOC_2010 = Parameter(default=19.0, unit="Sv")
-    MPI_AMOC_pi = Parameter(unit="Sv") # AMOC strength in MPI-ESM1.2-LR in 2015
-    AMOC_pi = Parameter(unit="Sv", default=20.) # preindustrial AMOC strength
+    T_AT_start = Parameter(default=0.854, unit="degC")
+    AMOC_start = Parameter(default=19.0, unit="Sv")
+    MPI_AMOC_pi = Parameter(unit="Sv") # AMOC strength in MPI-ESM1.2-LR in preindustrial simulation
+    AMOC_pi = Parameter(unit="Sv", default=20.) # preindustrial AMOC strength of respective model
     std_AMOC_pi = Parameter(unit="Sv", default=0.) # standard deviation of preindustrial AMOC strength
 
     function run_timestep(pp, vv, dd, tt)
         
         # AMOC decrease (measured in Sv)
-        vv.AMOC_decrease[tt] = pp.beta_AMOC[tt] * (pp.T_AT[tt] - pp.T_AT_2010)
-        vv.AMOC_strength[tt] = pp.AMOC_2010 - vv.AMOC_decrease[tt] + pp.std_AMOC_strength[tt]
+        vv.AMOC_decrease[tt] = pp.beta_AMOC[tt] * (pp.T_AT[tt] - pp.T_AT_start)
+        vv.AMOC_strength[tt] = pp.AMOC_start - vv.AMOC_decrease[tt] + pp.std_AMOC_strength[tt]
         
         # calculate CO2 release depending on AMOC strength while not imposing a maximum CO2 release
         vv.CO2_AMOC[tt] = pp.eta_AMOC * (pp.AMOC_pi + pp.std_AMOC_pi - vv.AMOC_strength[tt]) / pp.AMOC_pi * pp.MPI_AMOC_pi
@@ -40,22 +40,23 @@ function addAMOC_Carbon(model::Model, calibration::Union{Float64, Int, String}; 
 
     amoc_carbon = add_comp!(model, AMOC_Carbon, before=before, after=after, first=2010)
     
-    # calibrate AMOC decrease
+    # calibrate stylised AMOC decrease from 2010 to 2100
     if typeof(calibration) == Float64 || typeof(calibration) == Int
         T_AT_2010 = 0.854
-        amoc_carbon[:AMOC_2010] = AMOC_2010
+        amoc_carbon[:AMOC_start] = AMOC_2010
         println("Calibrating AMOC to stylised 2100 decrease of $calibration Sv, starting from an AMOC strength of $(AMOC_2010) Sv in 2010.")
         amoc_carbon[:beta_AMOC] = (calibration / (T_AT_2100 - T_AT_2010)) * ones(dim_count(model, :time))
         amoc_carbon[:MPI_AMOC_pi] = 18.9558
         amoc_carbon[:AMOC_pi] = AMOC_2010 # assume that AMOC strength in 2010 is the same as in preindustrial times
         amoc_carbon[:std_AMOC_pi] = 0.0
         amoc_carbon[:std_AMOC_strength] = zeros(dim_count(model, :time))
+    # calibrate to projection from 2015 to 2100
     elseif typeof(calibration) == String
         println("Calibrating AMOC to follow $calibration projection for $(scenario) from 2015 to 2100.")
-        beta_AMOC, AMOC_2010, T_AT_2010, AMOC_pi, MPI_AMOC_pi = calibrate_AMOC(model, calibration; scenario=scenario)
+        beta_AMOC, AMOC_2015, T_AT_2015, AMOC_pi, MPI_AMOC_pi = calibrate_AMOC(model, calibration; scenario=scenario)
         amoc_carbon[:beta_AMOC] = beta_AMOC
-        amoc_carbon[:AMOC_2010] = AMOC_2010
-        amoc_carbon[:T_AT_2010] = T_AT_2010
+        amoc_carbon[:AMOC_start] = AMOC_2015
+        amoc_carbon[:T_AT_start] = T_AT_2015
         amoc_carbon[:AMOC_pi] = AMOC_pi
         amoc_carbon[:MPI_AMOC_pi] = MPI_AMOC_pi
         amoc_carbon[:std_AMOC_pi] = 0.0 # will be updated during Monte Carlo runs
